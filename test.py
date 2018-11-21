3d16ecda90e70c4847f2ca7ad8a7fc038fd59348
SergioCorreal <sergio_a13@hotmail.es>
2018-11-21 18:44:14 +0000

    lalala

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-print("hola mundo cruel")
+print("holiiiiis")
 
 x = [1,2,3]
+y = [3,2,1]
+f = plt.figure()
+plt.plot(x,x)
+plt.plot(y,y)
+f.savefig("b.png")
