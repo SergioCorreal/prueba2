6ee7bbe958476d39a3378b2437afdd69a5096cdb
SergioCorreal <sergio_a13@hotmail.es>
2018-12-06 10:58:52 +0000

    esto es una pruebeira

--- .ipynb_checkpoints/nuevoo-checkpoint.py
+++ .ipynb_checkpoints/nuevoo-checkpoint.py
@@ -3,8 +3,9 @@
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-print("holiiiiis")
+import mapltotlib.pyplot as plt
+
+print("Saminamina ee wakaka e e ")
 
 x = [1,2,3]
 y = [3,2,1]
